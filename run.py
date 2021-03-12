eba40149fd38f9c592e3ce906ce82ae84cd19d28
maxthor500 <iraubuntu@gmail.com>
2021-03-12 14:14:34 +0000

    update function return with a render of inde.html template

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 
 # conventional instance of Flask
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    return render_template(index.html)
 
 
 if __name__ == "__main__":
